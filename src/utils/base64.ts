e03ff504ff1c19a7da2aca5432c928b752d362f2
sugamkuber <sugam7842692@gmail.com>
2024-07-24 23:03:17 +0530

    refactor: rm src

--- src/utils/base64.ts
+++
@@ -1,7 +0,0 @@
-export function base64UrlEncode(str: string): string {
-  return Buffer.from(str, 'utf8').toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-}
-
-export function base64UrlDecode(str: string): string {
-  return Buffer.from(str.replace(/-/g, '+').replace(/_/g, '/') + '='.repeat((4 - str.length % 4) % 4), 'base64').toString('utf8');
-}
